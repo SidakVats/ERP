f38e62e5503fbbab0dabb2658ca70aeef861bd08
SidakVats <sidakvats6499@gmail.com>
2024-05-11 16:30:35 +0530

    "removed more unnessary files"

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -26,7 +26,7 @@ const Sidebar = () => {
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
 
   // Define darkMode state and setDarkMode function
-  const [darkMode, setDarkMode] = React.useState(false);
+  // const [darkMode, setDarkMode] = React.useState(false);
 
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
